91a4f725acbaa9e584d6a991fc6f76758413c9ee
chowon986 <chowon986@gmail.com>
2022-01-17 23:22:52 +0900

    김초원 : 숙제 수정

--- HomeWork/00 CountFirst/00 CountFirst.cpp
+++ HomeWork/00 CountFirst/00 CountFirst.cpp
@@ -2,30 +2,37 @@
 
 int Len(const char* _Left)
 {
-    int i = 0;
-    while (*(_Left + i) != 0)
-    {
-        i += 1;
-    }
-    return i;
+	int i = 0;
+	while (*(_Left + i) != 0)
+	{
+		i += 1;
+	}
+	return i;
 }
 
 int CountFirst(const char* _Text, int _Start, const char* _FindStr)
 {
-    int a = Len(_Text);
+	int TextLen = Len(_Text);
+	int FindStrLen = Len(_FindStr);
 
-    for (int j =_Start; j<a; j+=1)
-    {
-        if (*(_Text+j) == *_FindStr)
-        {
-            return j-_Start;
-        }
-    }
-    return -1;
+	for (int j = _Start; j < TextLen; j += 1)
+	{
+		int i = 0;
+		while (*(_Text + j + i) == *(_FindStr + i) && i < FindStrLen - 1)
+		{
+			i += 1;
+		}
+
+		if (i == FindStrLen - 1)
+		{
+			return j - _Start;
+		}
+	}
+	return -1;
 }
 
 int main()
 {
-    int Count = CountFirst("aaa eee ttt asdfasd eee", 15, "eee");
-    printf_s("%d", Count);
+	int Count = CountFirst("aaa eee ttt asdfasd eee", 15, "eee");
+	printf_s("%d", Count);
 }
--- HomeWork/01 CountLast/01 CountLast.cpp
+++ HomeWork/01 CountLast/01 CountLast.cpp
@@ -2,28 +2,35 @@
 
 int Len(const char* _Left)
 {
-    int i = 0;
-    while (*(_Left + i) != 0)
-    {
-        i += 1;
-    }
-    return i;
+	int i = 0;
+	while (*(_Left + i) != 0)
+	{
+		i += 1;
+	}
+	return i;
 }
 int CountLast(const char* _Text, int _End, const char* _FindStr)
 {
-    int a = Len(_Text);
-    for (int j = _End; _End < (a + 1); j -= 1)
-    {
-        if (*(_Text + j) == *_FindStr)
-        {
-            return _End - j;
-        }
-        return -1;
-    }
+	int FindStrLen = Len(_FindStr);
+	int a = Len(_Text);
+	for (int j = _End; j >= 0; j -= 1)
+	{
+		int i = 0;
+		while (*(_Text + j - i) == *(_FindStr + i) && i < FindStrLen - 1)
+		{
+			i += 1;
+
+		}
+		if (i == FindStrLen - 1)
+		{
+			return _End - j;
+		}
+	}
+	return -1;
 }
 
 int main()
 {
-    int Count = CountLast("aaa eee asdfasd eee", 3, "eee");
-    printf_s("%d", Count);
+	int Count = CountLast("aaa eee ttt asdfasd eee", 15, "eee");
+	printf_s("%d", Count);
 }
