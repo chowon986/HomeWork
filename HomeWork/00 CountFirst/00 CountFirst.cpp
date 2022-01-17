578cf4a53467b31ed1ae9e803c268aeadbc874ec
chowon986 <chowon986@gmail.com>
2022-01-17 15:50:33 +0900

    김초원 : 오늘 분량 커밋

---
+++ HomeWork/00 CountFirst/00 CountFirst.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int Len(const char* _Left)
+{
+    int i = 0;
+    while (*(_Left + i) != 0)
+    {
+        i += 1;
+    }
+    return i;
+}
+
+int CountFirst(const char* _Text, int _Start, const char* _FindStr)
+{
+    int a = Len(_Text);
+
+    for (int j =_Start; j<a; j+=1)
+    {
+        if (*(_Text+j) == *_FindStr)
+        {
+            return j-_Start;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int Count = CountFirst("aaa eee ttt asdfasd eee", 15, "eee");
+    printf_s("%d", Count);
+}
---
+++ HomeWork/01 CountLast/01 CountLast.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int Len(const char* _Left)
+{
+    int i = 0;
+    while (*(_Left + i) != 0)
+    {
+        i += 1;
+    }
+    return i;
+}
+int CountLast(const char* _Text, int _End, const char* _FindStr)
+{
+    int a = Len(_Text);
+    for (int j = _End; _End < (a + 1); j -= 1)
+    {
+        if (*(_Text + j) == *_FindStr)
+        {
+            return _End - j;
+        }
+        return -1;
+    }
+}
+
+int main()
+{
+    int Count = CountLast("aaa eee asdfasd eee", 3, "eee");
+    printf_s("%d", Count);
+}
