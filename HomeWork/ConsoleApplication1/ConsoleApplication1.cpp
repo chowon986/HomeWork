58adcabbed95959f961e42d4e2b0cfe402be1e60
chowon986 <chowon986@gmail.com>
2022-01-26 16:38:13 +0900

    김초원 : 오늘 분량 커밋

---
+++ HomeWork/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+
+
+int Len2(const char& _Left)
+{
+	int i = 0;
+	while ((_Left + i) != 0)
+	{
+		i += 1;
+	}
+	return i;
+}
+
+int main()
+{
+
+	{
+		int b = Len2("12abCcEee");
+		std::cout << b;
+	}
+}
---
+++ HomeWork/operator++/00 operator++.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+using namespace std;
+
+class MyInt
+{
+public:
+	int MyValue = 0;
+
+public:
+	MyInt(int _Value)
+		:MyValue(_Value)
+	{
+
+	}
+
+public:
+	MyInt operator+();
+	MyInt operator+=(int);
+	MyInt& operator++();
+	MyInt operator++(int);
+};
+
+MyInt MyInt::operator+=(int a)
+{
+	MyValue = MyValue + a;
+	return MyValue;
+}
+
+MyInt MyInt::operator+()
+{
+	int a = 0;
+	int b = 0;
+	MyValue = a + b;
+	return MyValue;
+}
+MyInt& MyInt::operator++()
+{
+	MyValue = MyValue + 1;
+	return *this;
+}
+
+MyInt MyInt::operator++(int MyValue)
+{
+	MyInt temp = *this;
+	*this += 1;
+	return temp;
+}
+
+int main()
+{
+	int Value = 0;
+	int Result = 0;
+
+	Result = ++Value;
+	Result = 0;
+	Result = Value++;
+
+	MyInt MyValue = 0;
+	MyInt MyResult = 0;
+
+	MyResult = ++MyValue;
+	MyResult = 0;
+	MyResult = MyValue++;
+
+	int a = 0;
+}
+
+// MyResult에 MyValue값이 들어가야하는데
