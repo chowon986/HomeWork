184a40d01a825e4502d8e4d11ddad3ddc63345d7
chowon986 <chowon986@gmail.com>
2022-02-10 16:17:33 +0900

    김초원 : 오늘 분량 커밋

---
+++ HomeWork/02ConsoleScreen/Player.cpp
@@ -0,0 +1,55 @@
+#include "Player.h"
+#include <conio.h>
+#include "ConsoleGlobalInst.h"
+#include <stdlib.h>
+
+Player::Player(TextScreen* _Screen, const char* _Text)
+	: ConsoleObject(_Screen, _Text, _Screen->GetSize().GetHalfVector())
+{
+}
+
+Player::~Player()
+{
+}
+
+
+void Player::Update()
+{
+	// GlobalValue::AllMonsters
+
+	// AllMonsters[2];
+
+	// Pos_.x_ += 1; // Pos_.y_ += 0;
+	//   'a'
+	int Value = _getch();
+
+	switch (Value)
+	{
+	case 'a':
+	case 'A':
+		// 화면바깥으로 이동하면 이동이 안되게 해주세요.
+		SetMove({ -1, 0 });
+		break;
+	case 'd':
+	case 'D':
+		SetMove({ 1, 0 });
+		break;
+	case 'w':
+	case 'W':
+		SetMove({ 0, -1 });
+		break;
+	case 's':
+	case 'S':
+		SetMove({ 0, 1 });
+		break;
+	case 'q':
+	case 'Q':
+		exit(0);
+		break;
+	default:
+		break;
+	}
+
+
+}
+
