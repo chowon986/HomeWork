c4f7e990636524e2f7b5fc80a48407eca25eee85
chowon986 <chowon986@gmail.com>
2022-02-15 22:54:08 +0900

    김초원 : add nullchk

--- HomeWork/GameArray/GameArray.cpp
+++ HomeWork/GameArray/GameArray.cpp
@@ -7,113 +7,115 @@ using DataType = int;
 class GameArray
 {
 private:
-    int Size_;
-    DataType* ArrData_;
+	int Size_;
+	DataType* ArrData_;
 
 public:
-    int GetSize()
-    {
-        return Size_;
-    }
+	int GetSize()
+	{
+		return Size_;
+	}
 
-    DataType& operator[](int _Index)
-    {
-        if (Size_ <= _Index)
-        {
-            assert(false);
-        }
+	DataType& operator[](int _Index)
+	{
+		if (Size_ <= _Index)
+		{
+			assert(false);
+		}
 
-        return ArrData_[_Index];
-    }
+		return ArrData_[_Index];
+	}
 
-    inline void operator=(const GameArray& _Other)
-    {
-        ReSize(_Other.Size_);
+	inline void operator=(const GameArray& _Other)
+	{
+		ReSize(_Other.Size_);
 
-        for (size_t i = 0; i < this->Size_; i++)
-        {
-            this->ArrData_[i] = _Other.ArrData_[i];
-        }
-        return;
-    }
+		for (size_t i = 0; i < this->Size_; i++)
+		{
+			this->ArrData_[i] = _Other.ArrData_[i];
+		}
+		return;
+	}
 
-    inline void AllValueSetting(const DataType& Data)
-    {
-        for (size_t i = 0; i < this->Size_; i++)
-        {
-            this->ArrData_[i] = Data;
-        }
-    }
+	inline void AllValueSetting(const DataType& Data)
+	{
+		for (size_t i = 0; i < this->Size_; i++)
+		{
+			this->ArrData_[i] = Data;
+		}
+	}
 
-    inline void Release()
-    {
-        if (nullptr != ArrData_)
-        {
-            delete[] ArrData_;
-            ArrData_ = nullptr;
-        }
-    }
+	inline void Release()
+	{
+		if (nullptr != ArrData_)
+		{
+			delete[] ArrData_;
+			ArrData_ = nullptr;
+		}
+	}
 
-    void ReSize(unsigned int _Size)
-    {
-        DataType* NewArrData = new DataType[_Size];
+	void ReSize(unsigned int _Size)
+	{
+		DataType* NewArrData = new DataType[_Size];
 
-        int MinSize = 0;
-        if (Size_ > _Size)
-        {
-            MinSize = _Size;
-        }
-        else
-        {
-            MinSize = Size_;
-        }
+		int MinSize = 0;
+		if (Size_ > _Size)
+		{
+			MinSize = _Size;
+		}
+		else
+		{
+			MinSize = Size_;
+		}
 
-        for (int i = 0; i < MinSize; ++i)
-        {
-            NewArrData[i] = ArrData_[i];
-        }
-
-        Release();
-        ArrData_ = NewArrData;
-        Size_ = _Size;
-    }
+		if (NewArrData != nullptr)
+		{
+			for (int i = 0; i < MinSize; ++i)
+			{
+				NewArrData[i] = ArrData_[i];
+			}
+		}
+		Release();
+		ArrData_ = NewArrData;
+		Size_ = _Size;
+	}
 
 public:
-    GameArray(unsigned int _Size)
-        : ArrData_(nullptr)
-        , Size_(0)
-    {
-        ReSize(_Size);
-    }
+	GameArray(unsigned int _Size)
+		: ArrData_(nullptr)
+		, Size_(0)
+	{
+		ReSize(_Size);
+	}
 
-    ~GameArray()
-    {
-        Release();
-    }
+	~GameArray()
+	{
+		Release();
+	}
 };
 
 int main()
 {
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-    GameArray NewArray = GameArray(10);
-    // GameArray NewArray2 = GameArray(5);
-    GameArray NewArray2 = GameArray(20);
+	GameArray NewArray = GameArray(10);
+	// GameArray NewArray2 = GameArray(5);
+	GameArray NewArray2 = GameArray(20);
 
-    int Size = sizeof(GameArray);
+	int Size = sizeof(GameArray);
 
-    for (size_t i = 0; i < NewArray.GetSize(); i++)
-    {
-        NewArray[i] = i;
-    }
+	for (size_t i = 0; i < NewArray.GetSize(); i++)
+	{
+		NewArray[i] = i;
+	}
 
-    NewArray.AllValueSetting(0);
-    NewArray2 = NewArray;
+	NewArray.AllValueSetting(0);
+	NewArray2 = NewArray;
 
-    // NewArray2.ReSize(20);
+	// NewArray2.ReSize(20);
 
-    for (size_t i = 0; i < NewArray2.GetSize(); i++)
-    {
-        std::cout << NewArray2[i] << std::endl;
-    }
+	for (size_t i = 0; i < NewArray2.GetSize(); i++)
+	{
+		std::cout << NewArray2[i] << std::endl;
+	}
 }
